481485cebb67bcbc951ab44fb81a310ec89c0541
naolson2025 <naolson.1819@gmail.com>
2019-09-08 15:15:47 -0500

    Created add_blank_line branch to work on issue #8. Fixed issue #8 ready for testing from team member James

--- ui.py
+++ ui.py
@@ -25,12 +25,17 @@ def show_books(books):
      :param books: the book list """
 
     # TODO print a blank line before and after the book list or no books message
+    # Print blank line for readability
+    print()
 
     if books:
         for book in books:
             print(book)
     else:
         print('No books to display')
+
+    # Print blank line for readability
+    print()
 
 
 def get_book_info():
